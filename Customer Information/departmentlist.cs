108d76673b108f179a60381dd37af1f2c83e3aaa
ibroomall <ibroomall@arizona.edu>
2025-02-04 14:38:15 -0700

    departmentlist json reading changes

--- Customer Information/departmentlist.cs
+++ Customer Information/departmentlist.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-
-public class Departmentlist
+public class DepartmentList
 {
-    public Department[] Department { get; set; }
+    public string? Exception { get; set; }
+
+    public async Task GetInfo(string path)
+    {
+        try
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+        catch (Exception e)
+        {
+            this.Exception = e.ToString();
+            Console.WriteLine($"The process failed: {e}");
+        }
+
+        string fileName = Path.Combine(path, "departments.json");
+
+        await HTTP.DownloadFile(Globals.g_DepartmentJSONURL, fileName);
+      
+    }
+
+    public async Task<Department?> ReadJson()
+    {
+        if (!File.Exists(Globals.g_DepartmentJSONPath))
+        {
+            Console.WriteLine("Department data missing. Attempting to redownload...");
+            await GetInfo(Globals.g_DepartmentJSONPath);
+        }
+
+        using StreamReader reader = new StreamReader(Globals.g_DepartmentJSONPath);
+        string json = await reader.ReadToEndAsync();
+
+        try
+        {
+            return JsonConvert.DeserializeObject<Department>(json);
+        }
+        catch (Exception e)
+        {
+            this.Exception = e.ToString();
+            return null;
+        }
+    }
 }
 
 public class Department
@@ -17,10 +57,10 @@ public class Department
     public string Number { get; set; }
     public bool SupportKnown { get; set; }
     public bool SplitSupport { get; set; }
-    public Team[]? Team1 { get; set; }
-    public Team[]? Team2 { get; set; }
-    public Filerepo[]? filerepo { get; set; }
-    public string? notes { get; set; }
+    public Team? Team1 { get; set; }
+    public Team? Team2 { get; set; }
+    public Filerepo? Filerepo { get; set; }
+    public string? Notes { get; set; }
 }
 
 public class Team
--- Customer Information/globals.cs
+++ Customer Information/globals.cs
@@ -8,7 +8,9 @@ public class Globals
     public const string g_prodURL = "https://arizona.box.com/shared/static/27qy9jc64b0cpz4l6zzeu8pnri65y4d0.csv";
     const string g_testURL = "https://arizona.box.com/shared/static/osspbuwb9bgqknom1ts1uk2c173xgn5k.csv";
     const string g_QuickLinksURL = "https://arizona.box.com/shared/static/4jonapcgzw5lq2i8m40doma5x9t684de.json";
-    public readonly string g_SavePath = Environment.GetEnvironmentVariable("LocalAppData") + @"\Desktop_Support_App\";
+    public string g_SavePath = Environment.GetEnvironmentVariable("LocalAppData") + @"\Desktop_Support_App\";
     public const string g_versionTxt = "https://arizona.box.com/shared/static/5o9izr016qh0ywr8hsdk2f7vkijdl0xv.txt";
     public const string g_sharepointHome = "https://emailarizona.sharepoint.com/sites/TLC-desktopsupportapp/SitePages/ProjectHome.aspx";
+    public const string g_DepartmentJSONURL = "";
+    public const string g_DepartmentJSONPath = "";
 }
