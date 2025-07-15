2ffe4c80b38737ed09ebb6d89f218f41259c07ec
Isaac <ibroomall@arizona.edu>
2025-07-15 12:28:11 -0700

    Initial graph introduction

---
+++ Customer Information/graph.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Microsoft.Graph;
+public static class Graph
+{
+    public static GraphServiceClient InitializeGraphClient()
+    {
+        var credential = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions
+        {
+            ClientId = "your-client-id",
+            RedirectUri = new Uri("http://localhost"),
+            TenantId = "your-tenant-id"
+        });
+
+        return new GraphServiceClient(credential);
+    }
+
+    public static async Task<bool> TestGraphConnectionAsync(GraphServiceClient client)
+    {
+        try
+        {
+            var me = await client.Me.GetAsync();
+            Console.WriteLine($"Authenticated as: {me.DisplayName}");
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Graph error: {ex.Message}");
+            return false;
+        }
+    }
+}
