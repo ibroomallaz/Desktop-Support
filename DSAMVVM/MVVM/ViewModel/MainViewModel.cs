4d5047acdd0d09f64c9ce7efc499c54a5546b506
Isaac <ibroomall@arizona.edu>
2025-07-17 09:13:29 -0700

    Rest of pages setup

--- DSAMVVM/MVVM/ViewModel/MainViewModel.cs
+++ DSAMVVM/MVVM/ViewModel/MainViewModel.cs
@@ -12,10 +12,19 @@ namespace DSAMVVM.MVVM.ViewModel
         public RelayCommand HomeViewCommand { get; set; }
         public RelayCommand UserCommand { get; set; }
         public RelayCommand ComputerCommand { get; set; }
+        public RelayCommand GroupCommand { get; set; }
+        public RelayCommand EntraCommand { get; set; }
+        public RelayCommand LinksCommand { get; set; }
+        public RelayCommand AboutCommand { get; set; }
         public HomeViewModel HomeVM { get; set; }
         public ComputerViewModel ComputerVM { get; set; }
 
         public UserViewModel UserVM { get; set; }
+        public GroupViewModel GroupVM { get; set; }
+        public EntraViewModel EntraVM { get; set; }
+        public LinksViewModel LinksVM { get; set; }
+        public AboutViewModel AboutVM { get; set; }
+
         private object _currentView;
 
         public object CurrentView
@@ -31,6 +40,10 @@ namespace DSAMVVM.MVVM.ViewModel
             HomeVM = new HomeViewModel();
             UserVM = new UserViewModel(); 
             ComputerVM = new ComputerViewModel();
+            GroupVM = new GroupViewModel();
+            EntraVM = new EntraViewModel();
+            LinksVM = new LinksViewModel();
+            AboutVM = new AboutViewModel();
             CurrentView = HomeVM;
             HomeViewCommand = new RelayCommand(o => { 
                 CurrentView = HomeVM;
@@ -40,6 +53,18 @@ namespace DSAMVVM.MVVM.ViewModel
             });
             ComputerCommand = new RelayCommand(o => {
                 CurrentView = ComputerVM;
+            });
+            GroupCommand = new RelayCommand(o => {
+                CurrentView = GroupVM;
+            });
+            EntraCommand = new RelayCommand(o => {
+                CurrentView = EntraVM;
+            });
+            LinksCommand = new RelayCommand(o => {
+                CurrentView = LinksVM;
+            });
+            AboutCommand = new RelayCommand(o => {
+                CurrentView = AboutVM;
             });
         }
     }
