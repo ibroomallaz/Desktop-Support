878f2beb6be4f0a0e42b29673bc637662ee1bb3a
Isaac <ibroomall@arizona.edu>
2025-08-29 15:38:56 -0700

    UI Changes - Links

--- DSAMVVM/MVVM/View/LinksView.xaml.cs
+++ DSAMVVM/MVVM/View/LinksView.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Diagnostics;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DSAMVVM.MVVM.View
 {
-    /// <summary>
-    /// Interaction logic for LinksView.xaml
-    /// </summary>
     public partial class LinksView : UserControl
     {
         public LinksView()
@@ -25,9 +11,10 @@ namespace DSAMVVM.MVVM.View
             InitializeComponent();
         }
 
+        // open links -> default browser
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
-            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            Process.Start(new ProcessStartInfo
             {
                 FileName = e.Uri.AbsoluteUri,
                 UseShellExecute = true
