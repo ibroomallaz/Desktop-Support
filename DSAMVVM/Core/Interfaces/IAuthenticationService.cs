e776f8a2353f7998f0d794457a0c8bf20523cf91
Isaac <ibroomall@arizona.edu>
2026-06-12 15:19:22 -0700

    Initial testing

---
+++ DSAMVVM/Core/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿namespace DSAMVVM.Core.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        // Silent token acquisition via WAM broker, falling back to dsa:// interactive flow
+        Task<string> GetGraphAccessTokenAsync(string[] scopes);
+
+        // Ingests deep link query arguments to catch interactive authorization code bounces
+        void ProcessAuthRedirect(string url);
+    }
+}
