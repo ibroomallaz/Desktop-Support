7481ed1b6e949e78ad05ca9e911a1e3c68bab561
Isaac <ibroomall@arizona.edu>
2026-08-11 11:46:00 -0700

    Proper schema update

--- DSAMVVM/MVVM/Model/Schemas/SettingsMeta.cs
+++ DSAMVVM/MVVM/Model/Schemas/SettingsMeta.cs
@@ -1,12 +1,26 @@
-﻿namespace DSAMVVM.MVVM.Model.Schemas
+﻿using System.Runtime.Serialization;
 
+namespace DSAMVVM.MVVM.Model.Schemas
 {
     public sealed class SettingsMeta : JsonMetaBase
     {
         public SettingsMeta()
         {
-            SchemaVersion = DSAMVVM.MVVM.Model.Globals.g_SettingsSchema;
+            // This sets the version for brand new, factory-default files.
+            SchemaVersion = Globals.g_SettingsSchema;
+        }
 
+
+        //Schema Upgrade Enforcer
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            // If the JSON file we just read has an older schema version, 
+            // force it to upgrade to the current application standard in memory.
+            if (SchemaVersion < Globals.g_SettingsSchema)
+            {
+                SchemaVersion = Globals.g_SettingsSchema;
+            }
         }
     }
 }
