cd4a8450e04f4642d71784ddf139df47bdb3ce7e
Isaac <ibroomall@arizona.edu>
2026-08-11 12:38:01 -0700

    Properly enforce schema upgrades

--- DSAMVVM/MVVM/Model/Schemas/LinksMeta.cs
+++ DSAMVVM/MVVM/Model/Schemas/LinksMeta.cs
@@ -1,11 +1,22 @@
-﻿namespace DSAMVVM.MVVM.Model.Schemas
+﻿using System.Runtime.Serialization;
+
+namespace DSAMVVM.MVVM.Model.Schemas
 {
     public sealed class LinksMeta : JsonMetaBase
     {
         public LinksMeta()
         {
-            SchemaVersion = DSAMVVM.MVVM.Model.Globals.g_LinkJSONSchema;
+            SchemaVersion = Globals.g_LinkJSONSchema;
+        }
 
+        // Automatically enforces the current Links schema version when reading old files
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            if (SchemaVersion < Globals.g_LinkJSONSchema)
+            {
+                SchemaVersion = Globals.g_LinkJSONSchema;
+            }
         }
     }
 }
